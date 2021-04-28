f46aa331c30231aa2f0a9ff23bc912a1cc117af3
Nam Nguyen <nam@namnm.com>
2021-04-28 09:04:45 +0700

    Typo recieve -> receive (#40)

--- src/legacy/Routes.tsx
+++ src/legacy/Routes.tsx
@@ -9,7 +9,7 @@ import routes from './constants/routes.json'
 import App from './containers/App'
 import Dashboard from './components/Dashboard'
 import Send from './components/Send'
-import { Receive } from '../features/recieve'
+import { Receive } from '../features/receive'
 import LoadingScreen from './components/LoadingScreen'
 import AppState, {
   AddressBalance,
