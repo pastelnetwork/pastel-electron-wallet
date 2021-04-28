de0f7516b197cc60a68f65adb68cb554722f1eb3
Tuan Nguyen <titan.ngvtuan@gmail.com>
2021-04-28 17:11:53 +0700

    Fix issue CI failed

--- src/api/pastel-rpc/rpc.ts
+++ src/api/pastel-rpc/rpc.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import axios, { AxiosResponse } from 'axios'
 
 export type TRPCConfig = {
--- src/api/pastel-rpc/transactions.ts
+++ src/api/pastel-rpc/transactions.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import hex from 'hex-string'
 
 import { Transaction, TxDetail } from '../../declarations/app-state'
