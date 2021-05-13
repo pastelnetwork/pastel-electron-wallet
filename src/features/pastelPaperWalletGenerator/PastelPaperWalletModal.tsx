6d034bc51bed09838e2c94aca0a18f5ea0c5b7b0
Tuan Nguyen <titan.ngvtuan@gmail.com>
2021-05-13 17:18:20 +0700

    Fix pdf view

--- src/features/pastelPaperWalletGenerator/PastelPaperWalletModal.tsx
+++ src/features/pastelPaperWalletGenerator/PastelPaperWalletModal.tsx
@@ -21,120 +21,105 @@ type PastelPaperWalletModalProps = {
   }
 }
 
-export default function PastelPaperWalletModal({
-  info,
-}: PastelPaperWalletModalProps): JSX.Element {
-  const { address, privateKey, modalIsOpen } = useAppSelector(
-    state => state.pastelPaperWalletModal,
-  )
-  const dispatch = useAppDispatch()
+type PDFDocumentProps = {
+  address: string
+  privateKey: string
+  currencyName: string
+}
 
-  if (!modalIsOpen || !privateKey) {
-    return <></>
+const splitStringIntoChunks = (s: string, numChunks: number) => {
+  if (numChunks > s.length) {
+    return [s]
   }
 
-  const breakChar = '\u00ad'
-  const pdfStyles = StyleSheet.create({
-    viewer: {
-      width: '100%',
-      height: '100%',
-    },
-    page: {
-      flexDirection: 'row',
-    },
-    section: {
-      margin: 0,
-      padding: 20,
-      flexGrow: 1,
-    },
-    contentTop: {
-      display: 'flex',
-      width: '100%',
-      marginBottom: '40px',
-      paddingBottom: '20px',
-      borderBottom: '2px dashed #000',
-      flexDirection: 'row',
-    },
-    contentItem: {
-      margin: '0',
-    },
-    contentTitle: {
-      marginTop: '0',
-      marginBottom: '5px',
-      fontSize: '18px',
-      fontWeight: 'bold',
-    },
-    contentValue: {
-      marginTop: '0',
-      fontSize: '12px',
-      fontWeight: 'normal',
-      wordBreak: 'break-all',
-    },
-    marginTop20: {
-      marginTop: '20px',
-    },
-    mainContent: {
-      display: 'flex',
-      flexDirection: 'row',
-      width: '100%',
-      paddingBottom: '40px',
-      borderBottom: '2px solid #000',
-    },
-    topMedia: {
-      width: '35%',
-      maxWidth: '180px',
-      marginRight: '30px',
-    },
-    contentWrapper: {
-      width: '75%',
-    },
-    mainContentWrapper: {
-      width: '65%',
-    },
-    mainMedia: {
-      width: '35%',
-      maxWidth: '240px',
-      textAlign: 'right',
-    },
-    topImg: {
-      width: '100%',
-    },
-    mainImg: {
-      width: '100%',
-    },
-  })
-
-  const splitStringIntoChunks = (s: string, numChunks: number) => {
-    if (numChunks > s.length) {
-      return [s]
-    }
-
-    if (s.length < 16) {
-      return [s]
-    }
-
-    const chunkSize = Math.round(s.length / numChunks)
-    const chunks = []
-
-    for (let i = 0; i < numChunks - 1; i++) {
-      chunks.push(s.substr(i * chunkSize, chunkSize))
-    } // Last chunk might contain un-even length
-
-    chunks.push(s.substr((numChunks - 1) * chunkSize))
-    return chunks
+  if (s.length < 16) {
+    return [s]
   }
 
-  const generateFileName = () => {
-    const addr = splitStringIntoChunks(address, 6)[0]
-    const title = `PSL_Paper_Wallet__Shielded_Address_${addr}`
-    const date = new Date()
-    const dateTime = `${
-      date.getMonth() + 1
-    }_${date.getDate()}_${date.getFullYear()}__${date.getHours()}_${date.getMinutes()}`
-    return `${title}_${dateTime}.pdf`
-  }
+  const chunkSize = Math.round(s.length / numChunks)
+  const chunks = []
 
-  const PDFDocument = () => (
+  for (let i = 0; i < numChunks - 1; i++) {
+    chunks.push(s.substr(i * chunkSize, chunkSize))
+  } // Last chunk might contain un-even length
+
+  chunks.push(s.substr((numChunks - 1) * chunkSize))
+  return chunks
+}
+
+const pdfStyles = StyleSheet.create({
+  viewer: {
+    width: '100%',
+    height: '100%',
+  },
+  page: {
+    flexDirection: 'row',
+  },
+  section: {
+    margin: 0,
+    padding: 20,
+    flexGrow: 1,
+  },
+  contentTop: {
+    display: 'flex',
+    width: '100%',
+    marginBottom: '40px',
+    paddingBottom: '20px',
+    borderBottom: '2px dashed #000',
+    flexDirection: 'row',
+  },
+  contentItem: {
+    margin: '0',
+  },
+  contentTitle: {
+    marginTop: '0',
+    marginBottom: '5px',
+    fontSize: '18px',
+    fontWeight: 'bold',
+  },
+  contentValue: {
+    marginTop: '0',
+    fontSize: '12px',
+    fontWeight: 'normal',
+    wordBreak: 'break-all',
+  },
+  marginTop20: {
+    marginTop: '20px',
+  },
+  mainContent: {
+    display: 'flex',
+    flexDirection: 'row',
+    width: '100%',
+    paddingBottom: '40px',
+    borderBottom: '2px solid #000',
+  },
+  topMedia: {
+    width: '35%',
+    maxWidth: '180px',
+    marginRight: '30px',
+  },
+  contentWrapper: {
+    width: '75%',
+  },
+  mainContentWrapper: {
+    width: '65%',
+  },
+  mainMedia: {
+    width: '35%',
+    maxWidth: '240px',
+    textAlign: 'right',
+  },
+  topImg: {
+    width: '100%',
+  },
+  mainImg: {
+    width: '100%',
+  },
+})
+const breakChar = '\u00ad'
+
+function PDFDocument({ address, currencyName, privateKey }: PDFDocumentProps) {
+  return (
     <Document>
       <Page size='A4' style={pdfStyles.page}>
         <View style={pdfStyles.section}>
@@ -144,7 +129,7 @@ export default function PastelPaperWalletModal({
             </View>
             <View style={pdfStyles.contentWrapper}>
               <Text style={pdfStyles.contentTitle}>
-                {info.currencyName} Address (Sapling)
+                {currencyName} Address (Sapling)
               </Text>
               <Text style={pdfStyles.contentValue}>
                 {address.replace(/(.{40})/g, `$1${breakChar}`)}
@@ -161,7 +146,7 @@ export default function PastelPaperWalletModal({
               </View>
               <View style={pdfStyles.marginTop20}>
                 <Text style={pdfStyles.contentTitle}>
-                  {info.currencyName} Address (Sapling)
+                  {currencyName} Address (Sapling)
                 </Text>
                 <Text style={pdfStyles.contentValue}>
                   {address.replace(/(.{40})/g, `$1${breakChar}`)}
@@ -176,11 +161,40 @@ export default function PastelPaperWalletModal({
       </Page>
     </Document>
   )
+}
+
+export default function PastelPaperWalletModal({
+  info,
+}: PastelPaperWalletModalProps): JSX.Element {
+  const { address, privateKey, modalIsOpen } = useAppSelector(
+    state => state.pastelPaperWalletModal,
+  )
+  const dispatch = useAppDispatch()
+
+  if (!modalIsOpen || !privateKey) {
+    return <></>
+  }
+
+  const generateFileName = () => {
+    const addr = splitStringIntoChunks(address, 6)[0]
+    const title = `PSL_Paper_Wallet__Shielded_Address_${addr}`
+    const date = new Date()
+    const dateTime = `${
+      date.getMonth() + 1
+    }_${date.getDate()}_${date.getFullYear()}__${date.getHours()}_${date.getMinutes()}`
+    return `${title}_${dateTime}.pdf`
+  }
 
   const DownloadButton = () => (
     <button className={styles.btn_download} id='PDFDownloadBtn'>
       <PDFDownloadLink
-        document={<PDFDocument />}
+        document={
+          <PDFDocument
+            currencyName={info.currencyName}
+            address={address}
+            privateKey={privateKey}
+          />
+        }
         fileName={generateFileName()}
       />
     </button>
@@ -202,7 +216,11 @@ export default function PastelPaperWalletModal({
         </button>
         <DownloadButton />
         <PDFViewer style={pdfStyles.viewer}>
-          <PDFDocument />
+          <PDFDocument
+            currencyName={info.currencyName}
+            address={address}
+            privateKey={privateKey}
+          />
         </PDFViewer>
       </div>
     </Modal>
