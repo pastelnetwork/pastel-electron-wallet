e67b6cc9a48ce2f57bf8b6140bf40a8116806f2c
dinis ueldanov <realdinis@gmail.com>
2021-05-20 04:03:45 -0700

    fix: Update history router to Link

--- src/features/pastelStatistics/components/card/Card.tsx
+++ src/features/pastelStatistics/components/card/Card.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { useHistory } from 'react-router-dom'
 
 import bg1 from '../../assets/images/bg1.png'
 import bg2 from '../../assets/images/bg2.png'
@@ -16,29 +15,24 @@ type CardProps = {
   cardIndex: number
   title?: string
   subTitle?: string
-  routeUrl?: string
 }
 
 const backgroundImages = [bg1, bg2, bg3, bg4, bg5, bg6, bg7, bg8, bg9]
-
 export const Card = (props: CardProps): JSX.Element => {
-  const history = useHistory()
-  const { subTitle, routeUrl, cardIndex } = props
-
-  const handleCardClick = (routeUrl?: string) => {
-    if (routeUrl) {
-      history.push(routeUrl)
-    }
-  }
+  const { subTitle, cardIndex } = props
+  const defaultBg = ''
+  const selectedBg =
+    cardIndex > backgroundImages.length - 1
+      ? defaultBg
+      : backgroundImages[cardIndex]
 
   return (
     <div className={styles.cardWrap}>
       <div
         className={styles.cardContainer}
         style={{
-          backgroundImage: `url(${backgroundImages[cardIndex]})`,
+          backgroundImage: `url(${selectedBg})`,
         }}
-        onClick={() => handleCardClick(routeUrl)}
       ></div>
       <div className={styles.cardTitle}>{subTitle}</div>
     </div>
