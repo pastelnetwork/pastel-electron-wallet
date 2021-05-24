568d8ac28294f6f62b73e8406668e629e0311da8
Cristian <cinda.up@gmail.com>
2021-05-23 22:56:43 -0500

    Tailwind added

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+    // defaultLineHeights: true,
+    // standardFontWeights: true
+  },
+  purge: [],
+  theme: {
+    extend: {},
+    screens: {
+      sm: '414px',
+      md: '834px',
+      lg: '874px',
+      xl: '946px',
+      '2xl': '1440px',
+    },
+  },
+  variants: {},
+  plugins: [],
+}
