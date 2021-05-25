cb98123f4b905bab3a6ae13350bb1978023bf37d
crazyoptimist <crazyoptimist@mail.com>
2021-05-25 17:22:25 -0500

    update storybook setup * downgrade tailwind for storybook compatibility * include global styles to storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,26 +1,4 @@
-const path = require('path')
-
-// Storybook warning:
-// If you need PostCSS, include '@storybook/addon-postcss' in your '.storybook/main.js' file.
-// Suggested addon doesn't work unfortunately
-
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
-  webpackFinal: async config => {
-    config.module.rules.push({
-      test: /,css&/,
-      use: [
-        {
-          loader: 'postcss-loader',
-          options: {
-            ident: 'postcss',
-            plugins: [require('tailwindcss'), require('autoprefixer')],
-          },
-        },
-      ],
-      include: path.resolve(__dirname, '../src'),
-    })
-    return config
-  },
 }
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,5 @@
+import '!style-loader!css-loader!postcss-loader!../src/index.css'
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
