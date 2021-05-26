1525cba517249f2b7d985e8249db3bdb197bfe28
Dolet Bizhev <doletbizhev.dev@gmail.com>
2021-05-26 14:53:12 -0400

    create member stripe story

---
+++ src/features/members/stories/MemberStrip.stories.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+import mockAvatar from '../../../common/assets/images/avatar2-placeholder.png'
+import image from '../../../common/assets/images/member-image-placeholder.png'
+import MemberStripComponent, { MemberStripProps } from '../MemberStrip'
+
+const mockMemberStrips: MemberStripProps = {
+  memberCard: {
+    avatar: mockAvatar,
+    followers: 161,
+    name: 'Sally Fadel',
+  },
+  heighestSold: '1.700,000k',
+  totalSell: '1.500K',
+  images: [image, image, image, image],
+}
+export default {
+  title: 'Members/MemberStrip',
+}
+
+export const MemberStrip: React.FC = () => (
+  <div className=' text-text-gray900 bg-background-main h-full'>
+    <div className='p-2'>
+      <div className='bg-white p-5 rounded-lg'>
+        <MemberStripComponent {...mockMemberStrips} />
+      </div>
+    </div>
+  </div>
+)
