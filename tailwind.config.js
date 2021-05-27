08d57121156ecb3ee62f2872c513623581f55cb5
sanjanadhikari1 <sanjanadhikari1@gmail.com>
2021-05-27 15:48:35 -0400

    change boxshadow in the tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -41,15 +41,15 @@ module.exports = {
         '352px': '352px',
       },
       boxShadow: {
-        xSmall:
+        xs:
           '0px 0px 1px rgba(10, 22, 70, 0.06), 0px 32px 40px rgba(10, 22, 70, 0.12)',
-        small:
+        sm:
           '0px 0px 1px rgba(10, 22, 70, 0.06), 0px 3px 3px rgba(10, 22, 70, 0.1)',
-        medium:
+        md:
           '0px 0px 1px rgba(10, 22, 70, 0.06), 0px 6px 8px rgba(10, 22, 70, 0.1)',
-        large:
+        lg:
           '0px 0px 1px rgba(10, 22, 70, 0.06), 0px 16px 16px rgba(10, 22, 70, 0.1)',
-        xLarge:
+        xl:
           '0px 0px 1px rgba(10, 22, 70, 0.06), 0px 32px 40px rgba(10, 22, 70, 0.12)',
       },
       colors: {
