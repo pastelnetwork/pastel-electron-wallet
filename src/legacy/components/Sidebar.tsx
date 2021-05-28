84cff9820a16c903b87b9ef2f1f378c929ae2619
Cristian <cinda.up@gmail.com>
2021-05-27 21:41:59 -0500

    hide profile link on sidebar

--- src/legacy/components/Sidebar.tsx
+++ src/legacy/components/Sidebar.tsx
@@ -317,7 +317,7 @@ class Sidebar extends PureComponent<any, any> {
       exportPrivKeysModalIsOpen: false,
       exportedPrivKeys: null,
       privKeyInputValue: null,
-      showProfile: true,
+      showProfile: false,
     }
     this.setupMenuHandlers()
   } // Handle menu items
