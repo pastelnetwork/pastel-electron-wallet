d422096f5cb638e0dae126b013e70b2f1c244708
Cristian <cinda.up@gmail.com>
2021-05-31 09:14:59 +0300

    fixed color in profile general

--- src/features/profile/ProfileGeneral.tsx
+++ src/features/profile/ProfileGeneral.tsx
@@ -7,40 +7,40 @@ const ProfileGeneral = (): JSX.Element => {
       {/* First Row Group of General */}
       <div className='psl-w-full psl-py-10 psl-pt-4'>
         <div className='psl-flex psl-pt-2'>
-          <div className='psl-w-190px psl-text-sm psl-text-gray-4a'>
+          <div className='psl-w-190px psl-text-sm psl-text-gray-71'>
             Location
           </div>
-          <div className='psl-flex psl-flex-grow psl-text-gray-71'>
+          <div className='psl-flex psl-flex-grow psl-text-gray-4a'>
             <div>New York, US</div>
             <div className='psl-mx-auto psl--mt-2'>#121</div>
           </div>
         </div>
         <div className='psl-flex psl-pt-2'>
-          <div className='psl-w-190px psl-text-sm psl-text-gray-4a'>
+          <div className='psl-w-190px psl-text-sm psl-text-gray-71'>
             Language
           </div>
-          <div className='psl-flex psl-flex-grow psl-text-gray-71'>English</div>
+          <div className='psl-flex psl-flex-grow psl-text-gray-4a'>English</div>
         </div>
         <div className='psl-flex psl-pt-2'>
-          <div className='psl-w-190px psl-text-sm psl-text-gray-4a'>
+          <div className='psl-w-190px psl-text-sm psl-text-gray-71'>
             Categories
           </div>
-          <div className='psl-flex psl-flex-grow psl-text-gray-71'>
+          <div className='psl-flex psl-flex-grow psl-text-gray-4a'>
             motion graphics, illustration, <br />
             abstract
           </div>
         </div>
         <div className='psl-flex psl-pt-2'>
-          <div className='psl-w-190px psl-text-sm psl-text-gray-4a'>
+          <div className='psl-w-190px psl-text-sm psl-text-gray-71'>
             Buyer reputation
           </div>
-          <div className='psl-flex psl-flex-grow psl-text-gray-71 psl-flex-col xl:psl-flex-row'>
+          <div className='psl-flex psl-flex-grow psl-text-gray-4a psl-flex-col xl:psl-flex-row'>
             <Rate value={4.7} allowHalf={true} allowClear={false} />
             <div className='xl:psl-pl-2 text-gray-500'>4.89 reputation</div>
           </div>
         </div>
         <div className='psl-flex psl-pt-2'>
-          <div className='psl-w-190px psl-text-sm psl-text-gray-4a'>
+          <div className='psl-w-190px psl-text-sm psl-text-gray-71'>
             Buyer bans
           </div>
           <div className='psl-text-blue-400'>3</div>
@@ -49,28 +49,28 @@ const ProfileGeneral = (): JSX.Element => {
       {/* Second Row Group of General */}
       <div className='psl-w-full psl-py-10'>
         <div className='psl-flex psl-pt-2'>
-          <div className='psl-w-190px psl-text-sm psl-text-gray-4a'>
+          <div className='psl-w-190px psl-text-sm psl-text-gray-71'>
             Highest fee recieved
           </div>
-          <div className='psl-flex-grow psl-text-gray-71'>136,200,000k PSL</div>
+          <div className='psl-flex-grow psl-text-gray-4a'>136,200,000k PSL</div>
         </div>
         <div className='psl-flex psl-pt-2'>
-          <div className='psl-w-190px psl-text-sm psl-text-gray-4a'>
+          <div className='psl-w-190px psl-text-sm psl-text-gray-71'>
             Total sales amount
           </div>
-          <div className='psl-flex-grow psl-text-gray-71'>560,600,00k PSL</div>
+          <div className='psl-flex-grow psl-text-gray-4a'>560,600,00k PSL</div>
         </div>
         <div className='psl-flex psl-pt-2'>
-          <div className='psl-w-190px psl-text-sm psl-text-gray-4a'>
+          <div className='psl-w-190px psl-text-sm psl-text-gray-71'>
             Total items sold
           </div>
-          <div className='psl-flex-grow psl-text-gray-71'>14</div>
+          <div className='psl-flex-grow psl-text-gray-4a'>14</div>
         </div>
         <div className='psl-flex psl-pt-2'>
-          <div className='psl-w-190px psl-text-sm psl-text-gray-4a'>
+          <div className='psl-w-190px psl-text-sm psl-text-gray-71'>
             Top category persentage
           </div>
-          <div className='psl-flex-grow psl-text-gray-71'>
+          <div className='psl-flex-grow psl-text-gray-4a'>
             motion graphics 30%
           </div>
         </div>
@@ -78,10 +78,10 @@ const ProfileGeneral = (): JSX.Element => {
       {/* Third Row Group of General */}
       <div className='psl-w-full psl-py-10'>
         <div className='psl-flex psl-pt-2'>
-          <div className='psl-w-190px psl-text-sm psl-text-gray-4a'>Bio</div>
+          <div className='psl-w-190px psl-text-sm psl-text-gray-71'>Bio</div>
         </div>
         <div className='psl-flex psl-pt-2'>
-          <div className='psl-flex-grow psl-text-gray-71'>
+          <div className='psl-flex-grow psl-text-gray-4a'>
             I'am baby readymade mikshk tatooed actually activated charcoal
             godard listicle. Mumblecore cronut kickstarter, bushwick wolf copper
             mug woke chia put a bird on it viral gentrify keytar synth. Twee
