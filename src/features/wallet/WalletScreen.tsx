9f7ceef485009e02e43279d36ac12063dad50827
sanjanadhikari1 <sanjanadhikari1@gmail.com>
2021-05-31 00:40:04 -0400

    complete for the card in wallet page

--- src/features/wallet/WalletScreen.tsx
+++ src/features/wallet/WalletScreen.tsx
@@ -1,22 +1,65 @@
 import React from 'react'
 import { withRouter } from 'react-router'
+import NumberFormat from 'react-number-format'
 import placeholderIcon from '../../common/assets/icons/ico-placeholder.svg'
 
 const WalletScreen = () => {
+  const card_items = [
+    {
+      style: {
+        type: 'total_balance',
+        background: 'gray',
+        info: false,
+      },
+      psl: 210000,
+      icon: placeholderIcon,
+    },
+    {
+      style: {
+        type: 'transparent',
+        background: 'white',
+        info: true,
+      },
+      psl: 110000,
+      icon: placeholderIcon,
+    },
+    {
+      style: {
+        type: 'icon_placeholder',
+        background: 'gray',
+        info: true,
+      },
+      psl: 100000,
+      icon: placeholderIcon,
+    },
+  ]
   return (
-    <div className='psl-bg-gray-f8 pt-5 psl-px-60px'>
-      <div>
-        <div className='flex psl-w-427px psl-h-124px psl-pt-18px psl-pl-26px psl-border-gray-e7 border rounded-lg'>
-          <div>
-            <img className='w-92px h-87px' src={placeholderIcon} />
-          </div>
-          <div className='psl-pt-36px psl-pr-35px'>
-            <div className='text-2xl leading-6 psl-text-gray-4e'>
-              PSL 210,000
+    <div className='psl-bg-gray-f8 psl-pt-5 psl-px-60px'>
+      <div className='psl-flex'>
+        {card_items.map(card => (
+          <div className='psl-mr-18px'>
+            <div
+              className={`psl-flex psl-w-427px psl-h-124pxpsl-pl-26px psl-border-gray-e7 psl-border psl-rounded-lg ${
+                card.style.background == 'white' ? 'psl-bg-white' : ''
+              }`}
+            >
+              <div className='psl-pt-18px psl-pl-26px psl-pb-19px'>
+                <img className='psl-w-92px psl-h-87px' src={card.icon} />
+              </div>
+              <div className='psl-pt-36px psl-pl-35px'>
+                <div className='psl-text-2xl psl-leading-6 psl-text-gray-4e psl-pt-9'>
+                  PSL{' '}
+                  <NumberFormat
+                    value={card.psl}
+                    displayType={'text'}
+                    thousandSeparator={true}
+                  />
+                </div>
+                <div className='psl-text-gray-93 text-sm'>TOTAL BALENCE</div>
+              </div>
             </div>
-            <div className=''>TOTAL BALENCE</div>
           </div>
-        </div>
+        ))}
       </div>
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,45 +1,153 @@
 module.exports = {
-  future: {
-    // removeDeprecatedGapUtilities: true,
-    // purgeLayersByDefault: true,
-    // defaultLineHeights: true,
-    // standardFontWeights: true
-  },
-  purge: [],
+  purge: [
+    './src/**/*.ts',
+    './src/**/*.tsx',
+    './src/**/*.js',
+    './src/**/*.jsx',
+    './src/**/*.html',
+  ],
+  prefix: 'psl-',
   theme: {
+    screens: {
+      sm: '900px',
+      md: '1100px',
+      lg: '1300px',
+      xl: '1440px',
+      '2xl': '1700px',
+    },
+    fontFamily: {
+      body: 'Roboto, Arial, Helvetica, Helvetica Neue, serif',
+    },
     extend: {
       spacing: {
-        '2px': '2px',
-        '3px': '3px',
-        '6px': '6px',
-        '8px': '8px',
-        '13px': '13px',
-        '14px': '14px',
-        '15px': '15px',
-        '16px': '16px',
-        '18px': '18px',
-        '20px': '20px',
-        '22px': '22px',
-        '26px': '26px',
-        '28px': '28px',
-        '30px': '30px',
-        '35px': '35px',
-        '36px': '36px',
-        '37px': '37px',
-        '40px': '40px',
-        '41px': '41px',
-        '46px': '46px',
-        '50px': '50px',
-        '60px': '60px',
-        '66px': '66px',
-        '68px': '68px',
-        '70px': '70px',
-        '111px': '111px',
-        '112px': '112px',
-        '220px': '220px',
-        '300px': '300px',
-        '335px': '335px',
-        '352px': '352px',
+        //2px: 0.5 default
+        '3px': '0.188rem',
+        //4px: 1 default
+        //6px: 1.5 default
+        //8px: 2 default
+        //10px: 2.5 default
+        //12px: 3 default
+        '13px': '0.813rem',
+        //14px: 3.5 default
+        '15px': '0.938rem',
+        //16px: 4 default
+        '18px': '1.125rem',
+        '19px': '1.188rem',
+        //20px: 5 default
+        '22px': '1.375rem',
+        //24px: 6 default
+        '26px': '1.625rem',
+        //28px: 7 default
+        '30px': '1.875rem',
+        //32px: 8 default
+        '35px': '2.188rem',
+        //36px: 9 default
+        '37px': '2.313rem',
+        //40px: 10 default
+        '41px': '2.563rem',
+        //44px: 11 default
+        '46px': '2.875rem',
+        //48px: 12 default
+        '50px': '3.125rem',
+        //56px: 14 default
+        '60px': '3.75rem',
+        //64px: 16 default
+        '66px': '4.125rem',
+        '68px': '4.25rem',
+        '70px': '4.375',
+        //80px: 20 default
+        '82px': '5.125rem',
+        '87px': '5.438rem',
+        '92px': '5.75rem',
+        //96px: 24 default
+        '111px': '6.938rem',
+        //112px: 28 default
+        '124px': '7.75rem',
+        //128px: 32 default
+        //144px: 36 default
+        //160px: 40 default
+        //176px: 44 default
+        //192px: 48 default
+        //208px: 52 default
+        '220px': '13.75rem',
+        //224px: 56 default
+        //240px: 60 default
+        //256px: 64 default
+        //288px: 72 default
+        '300px': '18.75rem',
+        //320px: 80 default
+        '335px': '20.938rem',
+        '352px': '22rem',
+        //384px: 96 default
+        '427px': '26.688rem',
+      },
+      colors: {
+        success: {
+          DEFAULT: '#00D097',
+          pressed: '#00B282',
+          hover: '#00C28D',
+          background: '#E0F9F2',
+        },
+        warning: {
+          DEFAULT: '#FA9501',
+          pressed: '#E88A00',
+          hover: '#FFA21B',
+          background: '#FEF0DC',
+        },
+        error: {
+          DEFAULT: '#DA8AB8',
+          pressed: '#C85195',
+          hover: '#CE64A1',
+          background: '#FAEFF5',
+        },
+        blue: {
+          450: '#3F9AF7',
+          '3f': '#3F9AF7',
+        },
+        gray: {
+          f8: '#F8F9FA',
+          f7: '#F7F8F9',
+          e7: '#E7EAF2',
+          e6: '#E6E8EC',
+          b0: '#B0B7C3',
+          a0: '#A0AEC0',
+          71: '#718096',
+          '4a': '#4A5568',
+          '1a': '#1A202C',
+          110: '#F2F2F2',
+          35: '#353941',
+          33: '#334D6E',
+          '2d': '#2D3748',
+          '1d': '#1D263A',
+          14: '#141416',
+          '4e': '#4E5D78',
+          93: '#939EAB',
+        },
+        red: {
+          '7a': '#FF7A00',
+          ef: '#FFEFEB',
+        },
+        button: {
+          pressed: '#0E80F5',
+          hover: '#278DF6',
+          text: '#8E98A3',
+        },
+        navigation: {
+          DEFAULT: '#A6B0C2',
+          selected: '#334D6E',
+        },
+        tab: {
+          active: '#353945',
+          hover: '#F4F5F6',
+        },
+        line: {
+          DEFAULT: '#F2F4F7',
+        },
+        background: {
+          onboarding: '#FCFCFD',
+          main: '#F8F9FA',
+          modal: '#1A1A1A',
+        },
       },
       boxShadow: {
         xs:
@@ -53,37 +161,18 @@ module.exports = {
         xl:
           '0px 0px 1px rgba(10, 22, 70, 0.06), 0px 32px 40px rgba(10, 22, 70, 0.12)',
       },
-      colors: {
-        blue: {
-          '3f': '#3F9AF7',
-        },
-        gray: {
-          f8: '#F8F9FA',
-          f7: '#F7F8F9',
-          e7: '#E7EAF2',
-          e6: '#E6E8EC',
-          b0: '#B0B7C3',
-          a0: '#A0AEC0',
-          71: '#718096',
-          '4a': '#4A5568',
-          35: '#353941',
-          33: '#334D6E',
-          '2d': '#2D3748',
-          '1d': '#1D263A',
-          14: '#141416',
-        },
-        red: {
-          '7a': '#FF7A00',
-          ef: '#FFEFEB',
-        },
+      fontSize: {
+        h1: '2rem',
+        h2: '1.5rem',
+        h3: '1.375',
+        h4: '1.125',
+        h5: '1rem',
+        h6: '0.875rem',
+        15: '0.938rem',
       },
-    },
-    screens: {
-      sm: '900px',
-      md: '1100px',
-      lg: '1300px',
-      xl: '1440px',
-      '2xl': '1700px',
+      borderRadius: {
+        '4px': '4px',
+      },
     },
   },
   variants: {},
