adcd8c219edfa42ccf1b3ed4a2f7247326aa44c2
Sanjan Adhikari <sanjanadhikari1@gmail.com>
2021-06-07 13:03:51 -0400

    use correct 3rd operator

--- src/common/components/Toggle/Toggle.tsx
+++ src/common/components/Toggle/Toggle.tsx
@@ -34,8 +34,7 @@ const Toggle: React.FC<TToggleProps> = ({
             <div
               className={cn(
                 'dot absolute bg-white w-3 h-3 rounded-full',
-                !checked && 'left-1',
-                checked && 'right-1',
+                checked ? 'right-1' : 'right-1',
               )}
             ></div>
           </div>
