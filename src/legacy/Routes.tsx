10ac4de0db30ae3cc71689d836891a3ef4b360b7
Cristian <cinda.up@gmail.com>
2021-06-08 03:40:03 +0300

    change routes.tsx to fix module import issue

--- src/legacy/Routes.tsx
+++ src/legacy/Routes.tsx
@@ -50,7 +50,7 @@ import PastelUtils from '../common/utils/utils'
 import Creator from '../features/creator'
 import Collector from '../features/collector'
 import Nft from '../features/nft'
-import NFTMarketFeed from '../features/NFTMarket'
+import NFTMarketFeed from '../features/NFTMarket/NFTMarketFeed'
 import { app } from 'electron'
 
 export type TWalletInfo = {
