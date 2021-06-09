d5a5d87d5ad60c428aae7ca8f48316af8eca8d63
Cristian <cinda.up@gmail.com>
2021-06-09 06:16:27 +0300

    fix conflicts on Header

--- src/common/components/Header/HeaderScreen.tsx
+++ src/common/components/Header/HeaderScreen.tsx
@@ -11,7 +11,8 @@ import BellIcon from '../../assets/icons/ico-bell.svg'
 import MessageIcon from '../../assets/icons/ico-msg.svg'
 import SettingIcon from '../../assets/icons/ico-setting.svg'
 import UserIcon from '../../assets/icons/ico-user.svg'
-import cn from 'classnames'
+
+import Icon from '../Icon/Icon'
 
 interface IMenuType {
   name: string
@@ -20,7 +21,12 @@ interface IMenuType {
   style: string | undefined
 }
 
-const MenubarItem = ({ name, routeName, currentRoute, style }: IMenuType) => {
+const SidebarMenuItem = ({
+  name,
+  routeName,
+  currentRoute,
+  style,
+}: IMenuType) => {
   let isActive = false
 
   if (currentRoute.endsWith('app.html') || currentRoute === routeName) {
@@ -35,18 +41,12 @@ const MenubarItem = ({ name, routeName, currentRoute, style }: IMenuType) => {
 
   return (
     <div
-      // className={[
-      //   styles.headermenuitem,
-      //   activeColorClass,
-      //   'mr-8px md:mr-13px lg:mr-28px',
-      //   style,
-      // ].join(' ')}
-      className={cn(
+      className={[
         styles.headermenuitem,
         activeColorClass,
-        'mr-8px md:mr-13px lg:mr-28px font-medium h-22px',
+        'mr-8px md:mr-13px lg:mr-28px',
         style,
-      )}
+      ].join(' ')}
     >
       <Link to={routeName}>
         <span className={activeColorClass}>{name}</span>
@@ -58,10 +58,10 @@ const MenubarItem = ({ name, routeName, currentRoute, style }: IMenuType) => {
 const SearhBar = () => {
   const placeholder = 'Search creator or NFT'
   return (
-    <div className='psl-flex psl-relative'>
+    <div className='flex relative'>
       <img width='16' className={styles.searchIconPosition} src={searchIcon} />
       <input
-        className=' placeholder-gray-b0 h-41px bg-gray-110 rounded-full pl-46px md:w-300px lg:w-300px xl:w-352px'
+        className='h-41px bg-gray-110 rounded-full pl-46px md:w-300px lg:w-300px xl:w-352px'
         placeholder={`${placeholder}`}
       />
     </div>
@@ -101,30 +101,30 @@ const Header = (props: PropsType) => {
     },
   ]
 
-  const menubar_items = [
+  const sidebar_items = [
     {
       name: 'Dashboard',
       routeName: routes.DASHBOARD,
       currentRoute: location.pathname,
-      style: 'xl:psl-mr-35px',
+      style: 'xl:mr-35px',
     },
     {
       name: 'NFTs',
-      routeName: routes.MARKET,
+      routeName: routes.SEND,
       currentRoute: location.pathname,
-      style: 'xl:psl-mr-37px',
+      style: 'xl:mr-37px',
     },
     {
       name: 'Members',
-      routeName: routes.MEMBERS,
+      routeName: routes.RECEIVE,
       currentRoute: location.pathname,
-      style: 'xl:psl-mr-28px',
+      style: 'xl:mr-28px',
     },
     {
       name: 'Wallet',
       routeName: routes.TRANSACTIONS,
       currentRoute: location.pathname,
-      style: 'xl:psl-mr-35px',
+      style: 'xl:mr-35px',
     },
     {
       name: 'Portfolio',
@@ -133,14 +133,14 @@ const Header = (props: PropsType) => {
     },
   ]
   return (
-    <div className='flex items-center h-66px bg-white justify-between md:text-h6 lg:text-15 xl:text-h5 font-display border-b border-gray-e6'>
+    <div className='flex items-center h-66px bg-white justify-between md:text-14px lg:text-15px xl:text-16px font-display'>
       <div className='flex items-center'>
         <div className='ml-20px md:ml-30px lg:ml-50px xl:ml-60px mr-13px md:mr-20px lg:mr-30px xl:mr-40px w-30px md:w-36px h-30px md:h-36px'>
           <img src={Logo} alt='logo' />
         </div>
         <div className='flex'>
-          {menubar_items.map((item, index) => (
-            <MenubarItem
+          {sidebar_items.map((item, index) => (
+            <SidebarMenuItem
               key={index}
               name={item.name}
               routeName={item.routeName}
@@ -148,27 +148,25 @@ const Header = (props: PropsType) => {
               style={item.style}
             />
           ))}
-          <div className='md:psl-ml-13px lg:psl-ml-40px xl:psl-ml-50px'>
-            <Link to='#' className='psl-flex psl-items-center'>
+          <div className='md:ml-13px lg:ml-40px xl:ml-50px'>
+            <Link to='#' className='flex items-center'>
               <img
                 src={addBtn}
-                className='w-20px h-20px mr-2px md:mr-2'
+                className='w-20px h-20px mr-2px md:mr-8px'
                 alt='add button'
               ></img>
-              <span className='psl-text-blue-450 psl-whitespace-nowrap'>
-                new NFT
-              </span>
+              <span className='text-blue-450 whitespace-nowrap'>new NFT</span>
             </Link>
           </div>
         </div>
-        <div className='psl-ml-20px md:psl-ml-40px lg:psl-ml-50px xl:psl-ml-68px md:psl-mr-8px lg:psl-mr-8px xl:psl-mr-8px'>
+        <div className='ml-20px md:ml-40px lg:ml-50px xl:ml-68px md:mr-8px lg:mr-8px xl:mr-8px'>
           <SearhBar />
         </div>
       </div>
-      <div className='flex items-center mr-33px'>
+      <div className='flex items-center mr-33'>
         {icons.map((icon, index) => (
-          <div className='psl-mr-20px md:psl-mr-26px' key={index}>
-            <img src={icon.src} />
+          <div className='mr-20px md:mr-26px' key={index}>
+            <Icon src={icon.src} variant={icon.variant} />
           </div>
         ))}
       </div>
