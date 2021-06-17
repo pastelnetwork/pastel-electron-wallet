e2d0268969728ac93ecd5783901bc082f50ae725
prodev1101 <anton.matyunin1101@outlook.com>
2021-06-17 13:10:57 +0800

    disable onboarding show and fix eslint

--- src/legacy/Routes.tsx
+++ src/legacy/Routes.tsx
@@ -499,7 +499,7 @@ class RouteApp extends React.Component<any, any> {
         <AboutModal />
         <SquooshToolModal />
         <GlitchImageModal />
-        { <Header />}
+        {info?.version && <Header />}
         <div className='flex-grow overflow-auto'>
           <Switch>
             <Route path={routes.MARKET} render={() => <NFTMarketFeed />} />
