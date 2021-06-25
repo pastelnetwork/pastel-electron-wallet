79ecb3de1cb516be1083d8186f9a4dab709b90ca
muhadovic <49214944+muhadovic@users.noreply.github.com>
2021-06-25 19:44:04 +0200

    Merged feature/notificationModal_40112315 (#161)

---
+++ src/features/dashboard/dashboardModals/notificationModal.data.ts
@@ -0,0 +1,126 @@
+import { TNotification } from './notificationModal'
+
+const notificationData: TNotification[] = [
+  {
+    id: 1,
+    type: 'system',
+    title: 'New listing: Diamonds in the sky by followed Bnksy64',
+    status: 'unread',
+  },
+  {
+    id: 2,
+    type: 'bidding',
+    title: 'You’ve receiver 10,000,000 for the sold item',
+    status: 'unread',
+  },
+  {
+    id: 3,
+    type: 'wallet',
+    title: 'Remiders: bookmarked auction starts in 10 minutes',
+    status: 'unread',
+  },
+  {
+    id: 4,
+    type: 'system',
+    title: 'New listing: Diamonds in the sky by followed Bnksy64',
+    status: 'read',
+  },
+  {
+    id: 5,
+    type: 'system',
+    title: 'New listing: Diamonds in the sky by followed Bnksy64',
+    status: 'read',
+  },
+  {
+    id: 6,
+    type: 'wallet',
+    title: 'New listing: Diamonds in the sky by followed Bnksy64',
+    status: 'read',
+  },
+  {
+    id: 7,
+    type: 'system',
+    title: 'New listing: Diamonds in the sky by followed Bnksy64',
+    status: 'read',
+  },
+  {
+    id: 8,
+    type: 'wallet',
+    title: 'New listing: Diamonds in the sky by followed Bnksy64',
+    status: 'read',
+  },
+  {
+    id: 9,
+    type: 'system',
+    title: 'New listing: Diamonds in the sky by followed Bnksy64',
+    status: 'read',
+  },
+  {
+    id: 10,
+    type: 'system',
+    title: 'New listing: Diamonds in the sky by followed Bnksy64',
+    status: 'read',
+  },
+  {
+    id: 11,
+    type: 'system',
+    title: 'New listing: Diamonds in the sky by followed Bnksy64',
+    status: 'read',
+  },
+  {
+    id: 12,
+    type: 'wallet',
+    title: 'New listing: Diamonds in the sky by followed Bnksy64',
+    status: 'read',
+  },
+  {
+    id: 13,
+    type: 'system',
+    title: 'New listing: Diamonds in the sky by followed Bnksy64',
+    status: 'read',
+  },
+  {
+    id: 14,
+    type: 'bidding',
+    title: 'New listing: Diamonds in the sky by followed Bnksy64',
+    status: 'read',
+  },
+  {
+    id: 15,
+    type: 'system',
+    title: 'New listing: Diamonds in the sky by followed Bnksy64',
+    status: 'read',
+  },
+  {
+    id: 16,
+    type: 'bidding',
+    title: 'New listing: Diamonds in the sky by followed Bnksy64',
+    status: 'read',
+  },
+  {
+    id: 17,
+    type: 'system',
+    title: 'New listing: Diamonds in the sky by followed Bnksy64',
+    status: 'read',
+  },
+  {
+    id: 18,
+    type: 'bidding',
+    title: 'New listing: Diamonds in the sky by followed Bnksy64',
+    status: 'read',
+  },
+  {
+    id: 19,
+    type: 'system',
+    title: 'New listing: Diamonds in the sky by followed Bnksy64',
+    status: 'read',
+  },
+  {
+    id: 20,
+    type: 'system',
+    title: 'New listing: Diamonds in the sky by followed Bnksy64',
+    status: 'read',
+  },
+]
+
+export default notificationData
