8bf1cd07e70363490ddaca6d645d82913c738f37
Sanjan Adhikari <sanjanadhikari1@gmail.com>
2021-06-28 10:39:20 -0400

    add linechart config

---
+++ src/common/components/LineChart/LineChart.stories.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Story, Meta } from '@storybook/react'
+import LineChart from './LineChart'
+
+export const LikeDefalt: Story = () => <LineChart />
+
+export default {
+  component: LineChart,
+  title: 'LineChart',
+} as Meta
---
+++ src/common/components/LineChart/LineChart.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const LineChart = (): JSX.Element => {
+  return <div className='chart'></div>
+}
+
+export default LineChart
