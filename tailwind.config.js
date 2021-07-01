005cda448b43b56ec587f3b5cc2fb8e6877ca00e
ArthurVogelsang <57923845+ArthurVogelsang@users.noreply.github.com>
2021-07-01 11:00:38 +0300

    Updated NFT Cards (#173)
    https://freedcamp.com/view/2956183/tasks/panel/task/40753587
    Update NFT Card

--- tailwind.config.js
+++ tailwind.config.js
@@ -178,6 +178,8 @@ module.exports = {
           f8: '#F8FBFF',
           e5: '#E5F1FF',
           eb: '#EBF2FF',
+          79: '#79A8FA',
+          68: '#687EFE',
           37: '#3754DB',
         },
         gray: {
@@ -194,6 +196,7 @@ module.exports = {
           e6: '#E6E8EC',
           e4: '#E4E4E4',
           e1: '#E1E4E8',
+          ef: '#EFF5FD',
           dd: '#DDE0E3',
           cd: '#CDD2DB',
           b0: '#B0B7C3',
@@ -238,6 +241,7 @@ module.exports = {
           81: '#E98160',
           63: '#FE634C',
           75: '#FF754C',
+          ff: '#FFCE14',
         },
         yellow: {
           ff: '#FFBE3D',
@@ -250,6 +254,8 @@ module.exports = {
           68: '#68CC67',
           45: '#45B36B',
           38: '#38CB89',
+          16: '#16B4EB',
+          23: '#23E73C',
         },
         button: {
           DEFAULT: '#3F9AF7',
@@ -325,6 +331,7 @@ module.exports = {
         h4: '1.125rem',
         h5: '1rem',
         h6: '0.875rem',
+        '8px': '0.5rem',
         '9px': '0.5625rem',
         '10px': '0.625rem',
         '12px': '0.75rem',
