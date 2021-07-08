efb026e4b514611b37b48e09459d338bbd0c123f
ngvtuan <titan.ngvtuan@gmail.com>
2021-07-07 19:55:12 -0700

    Feature/import export address labels 40895968 (#181)
    * we need to include in your QRcode videos any user defined address labels
    
    * remove unused code
    
    * remove icon unused
    
    * removed Icon styled-component

--- src/common/components/Icons/Caret.tsx
+++ src/common/components/Icons/Caret.tsx
@@ -5,6 +5,7 @@ type TDir = 'top' | 'right' | 'bottom' | 'left'
 
 export type TCaretProps = TIconProps & {
   to?: TDir
+  variant?: string
 }
 
 const rotate: Record<TDir, number> = {
@@ -18,6 +19,7 @@ export const Caret: React.FC<TCaretProps> = ({
   size,
   to = 'right',
   className,
+  variant,
 }) => {
   const style = { transform: `rotate(${rotate[to]}deg)` }
 
@@ -30,13 +32,23 @@ export const Caret: React.FC<TCaretProps> = ({
       fill='none'
       xmlns='http://www.w3.org/2000/svg'
     >
-      <path
-        d='M1 1L5 5L9 1'
-        stroke='#B0B7C3'
-        strokeWidth='2'
-        strokeLinecap='round'
-        strokeLinejoin='round'
-      />
+      {variant === 'full' ? (
+        <path
+          opacity='0.5'
+          fillRule='evenodd'
+          clipRule='evenodd'
+          d='M1.05208 0C0.684274 0 0.511432 0.454606 0.786334 0.698964L4.73425 4.20823C4.88581 4.34294 5.11419 4.34294 5.26575 4.20823L9.21367 0.698964C9.48857 0.454606 9.31573 0 8.94792 0H1.05208Z'
+          fill='#4A5568'
+        />
+      ) : (
+        <path
+          d='M1 1L5 5L9 1'
+          stroke='#B0B7C3'
+          strokeWidth='2'
+          strokeLinecap='round'
+          strokeLinejoin='round'
+        />
+      )}
     </svg>
   )
 }
