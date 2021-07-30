e881de7d71fa3b8042f3351381305fca698246c4
Tuan Nguyen <titan.ngvtuan@gmail.com>
2021-07-30 12:20:19 +0700

    Updated the DateRangeSelector component Fixed warning issues - Fixed pdf render warning - Fixed postcss warning - Fixed QR code warning - Fixed react modal warning - Fixed select component warning - Fixed babel warning - Fixed text auto-size warning - Fixed file-loader warning - Fixed storybook warning - Fixed file-loaded warning - Fixed ccs loader warning - Fixed node-loader - Fixed style loader

--- .storybook/main.js
+++ .storybook/main.js
@@ -36,7 +36,8 @@ module.exports = {
     ...options,
 
     plugins: [
-      '@babel/plugin-proposal-class-properties',
+      ['@babel/plugin-proposal-class-properties', { loose: true }],
+      ['@babel/plugin-proposal-private-methods', { loose: true }],
       '@babel/plugin-proposal-optional-chaining',
     ],
   }),
