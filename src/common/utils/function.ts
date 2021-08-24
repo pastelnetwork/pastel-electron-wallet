6fe10f48602b24ec332289f831bdaf55a09cd83e
crazyoptimist <crazyoptimist@mail.com>
2021-08-24 15:12:29 -0400

    Change debounce function to remove eslint comment

--- src/common/utils/function.ts
+++ src/common/utils/function.ts
@@ -1,13 +1,14 @@
-// eslint-disable-next-line @typescript-eslint/ban-types
-export const debounce = <T extends Function>(fn: T, wait: number): T => {
-  let timeout: NodeJS.Timeout | undefined
-  return (function (...args: unknown[]) {
-    if (timeout) {
-      clearTimeout(timeout)
+export function debounce<T extends unknown[], U>(
+  callback: (...args: T) => U,
+  wait: number,
+): (...args: T) => void {
+  let timer: NodeJS.Timeout | undefined
+
+  return (...args: T): void => {
+    if (timer) {
+      clearTimeout(timer)
     }
-    timeout = setTimeout(function () {
-      timeout = undefined
-      fn(...args)
-    }, wait)
-  } as unknown) as T
+
+    timer = setTimeout(() => callback(...args), wait)
+  }
 }
