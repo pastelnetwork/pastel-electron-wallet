eadad2a5e096a8609aad34e0e018f48740271b3d
Tuan Nguyen <titan.ngvtuan@gmail.com>
2021-11-23 16:41:14 +0700

    Inconsistent function style for functional components

--- src/common/components/Modal/Modal.stories.tsx
+++ src/common/components/Modal/Modal.stories.tsx
@@ -2,10 +2,7 @@ import React, { useCallback } from 'react'
 import { Story, Meta } from '@storybook/react'
 import Modal, { TModal } from './Modal'
 
-const Template: Story<TModal> = function StoryModal({
-  isOpen,
-  ...args
-}: TModal) {
+const Template: Story<TModal> = ({ isOpen, ...args }) => {
   const [showModal, setShowModal] = React.useState(isOpen)
 
   const onClick = useCallback(() => {
