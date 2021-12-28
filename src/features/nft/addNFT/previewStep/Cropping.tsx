8d1d84637e59b0b3905c3262c6ed04fd687ec263
Tuan Nguyen <titan.ngvtuan@gmail.com>
2021-12-28 14:34:32 +0700

    - Regular Expression Denial of Service (ReDoS)

--- src/features/nft/addNFT/previewStep/Cropping.tsx
+++ src/features/nft/addNFT/previewStep/Cropping.tsx
@@ -85,8 +85,7 @@ export default function Cropping({
 
     const dataURLtoFile = (dataUrl: string, filename: string) => {
       const arr = dataUrl.split(',')
-      const search = new RegExp(/:(.*?);/)
-      const mime = search.exec(arr[0])
+      const mime = arr[0].split(/:(.*?);/)
       const bstr = atob(arr[1])
       let n = bstr.length
       const u8arr = new Uint8Array(n)
