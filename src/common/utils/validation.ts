6b33c9104d607ef6107d799a68293840b1be569f
Tuan Nguyen <titan.ngvtuan@gmail.com>
2022-01-06 14:34:51 +0700

    Inefficient regular expression

--- src/common/utils/validation.ts
+++ src/common/utils/validation.ts
@@ -1,12 +1,4 @@
 export const validURL = (str: string): boolean => {
-  const pattern = new RegExp(
-    '^(https?:\\/\\/)?' +
-      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' +
-      '((\\d{1,3}\\.){3}\\d{1,3}))' +
-      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' +
-      '(\\?[;&a-z\\d%_.~+=-]*)?' +
-      '(\\#[-a-z\\d_]*)?$',
-    'i',
-  )
+  const pattern = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\\+.~#?&//=]*)/g
   return !!pattern.test(str)
 }
