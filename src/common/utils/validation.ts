3a66e07c796a6cff3ba3f9928eb4d807ac530185
Tuan Nguyen <titan.ngvtuan@gmail.com>
2022-01-06 14:42:25 +0700

    Regular Expression Denial of Service (ReDoS)

--- src/common/utils/validation.ts
+++ src/common/utils/validation.ts
@@ -1,4 +1,8 @@
-export const validURL = (str: string): boolean => {
-  const pattern = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\\+.~#?&//=]*)/g
-  return !!pattern.test(str)
+export const validURL = (url: string): boolean => {
+  try {
+    new URL(url)
+  } catch (e) {
+    return false
+  }
+  return true
 }
